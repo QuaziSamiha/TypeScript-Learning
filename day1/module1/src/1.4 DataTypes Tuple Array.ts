87a6a4e370e59604760995858250d559ea3b37ad
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-05-02 07:58:30 +0600

    data types, tuple, array basic

--- day1/module1/src/1.4 DataTypes Tuple Array.ts
+++ day1/module1/src/1.4 DataTypes Tuple Array.ts
@@ -1,30 +1,32 @@
-// basic data types 01.05.24
-// string
-let firstName: string = "samiha";
-// number
-let roll: number = 1703155;
-// boolean
-let isAdmin: boolean = false;
-// undefined
-let x: undefined = undefined;
-// null
-let y: null = null;
+{
+  // basic data types 01.05.24
+  // string
+  let firstName: string = "samiha";
+  // number
+  let roll: number = 1703155;
+  // boolean
+  let isAdmin: boolean = false;
+  // undefined
+  let x: undefined = undefined;
+  // null
+  let y: null = null;
 
-// if you not defined, then compiler will define it as "any" type
-let d; // not recommended
-d = 155;
-d = "pochu";
-d = true;
+  // if you not defined, then compiler will define it as "any" type
+  let d; // not recommended
+  d = 155;
+  d = "pochu";
+  d = true;
 
-// array
-let friends: string[] = ["rehel", "vondul"];
-// frinends.push(7) // it will show an error
-let rollList: number[] = [1, 3];
-rollList.push(7);
-console.log(rollList);
+  // array
+  let friends: string[] = ["rehel", "vondul"];
+  // frinends.push(7) // it will show an error
+  let rollList: number[] = [1, 3];
+  rollList.push(7);
+  console.log(rollList);
 
-// tuple --> special kind of array --> order --> type of values
-let coordinates : [number, number] = [1,5]
-let ageName : [number, string] = [50, 'Sam']
-// ageName[0] = 'Tasnim' // will show an error
-ageName[0] = 55
+  // tuple --> special kind of array --> order --> type of values
+  let coordinates: [number, number] = [1, 5];
+  let ageName: [number, string] = [50, "Sam"];
+  // ageName[0] = 'Tasnim' // will show an error
+  ageName[0] = 55;
+}
