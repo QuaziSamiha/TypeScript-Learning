dcc7702ef05534d6db2cebd6b7f8b0099bb087a9
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-05-03 23:24:04 +0600

    mapped types

---
+++ module2/2.23.ts
@@ -0,0 +1,27 @@
+{
+  // 03.05.24
+  // mapped types
+
+  const arrOfNumbers: number[] = [1, 4, 5];
+  //   const arrOfStrings: string[] = ['1', '4', '5'];
+  const arrOfStrings: string[] = arrOfNumbers.map((number) =>
+    number.toString()
+  );
+  console.log(arrOfStrings);
+
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+
+  //   type AreaString = {
+  //     height: string;
+  //     width: string;
+  //   };
+
+  type AreaString = {
+    // [key in "height" | "width"]: string;
+    [key in keyof AreaNumber]: string;
+    // [key in keyof AreaNumber]: boolean;
+  };
+}
